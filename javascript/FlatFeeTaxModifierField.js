7d62bc569d38fe22b17c50776a296f236a404902
Frank Mullenger <frankmullenger@gmail.com>
2012-11-14 09:15:56 +1300

    BUGFIX: Listening to incorrect dropdown.

--- javascript/FlatFeeTaxModifierField.js
+++ javascript/FlatFeeTaxModifierField.js
@@ -1,6 +1,6 @@
 (function($) { 
     $(document).ready(function() { 
-    	$('#CheckoutForm_OrderForm_Shipping-Country').live('change', updateOrderFormCartAJAX);
+    	$('#CheckoutForm_OrderForm_Shipping-CountryCode').live('change', updateOrderFormCartAJAX);
     	$('.modifier-set-field select').live('change', updateOrderFormCartAJAX);
     })
 })(jQuery);
